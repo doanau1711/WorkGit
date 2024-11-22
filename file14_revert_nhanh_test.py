132797ea43fa5193925877f5ac72a4ab2782063f
DoanLongAu <audoan@gmail.com>
2024-11-22 17:16:27 +0700

    Revert "Add new file14_revert_nhanh_test.py"
    This reverts commit 0ae49baf88cc59443950efb5a0b42d3b9a64c6f5.

--- file14_revert_nhanh_test.py
+++
@@ -1,5 +0,0 @@
-print("Tạo một cái pull request và merge request")
-print("Tạo một cái nội dung cho pull request")
-print("Tìm hiểu reset and revert")
-print("Nội dung cho revert")
-print("Cần thêm nội dung cho revert")
