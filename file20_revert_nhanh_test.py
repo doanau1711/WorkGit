deee0dec2469725bfcce9070973a3d0e2bf071b2
DoanLongAu <audoan@gmail.com>
2024-11-25 13:46:17 +0700

    Add new file20

---
+++ file20_revert_nhanh_test.py
@@ -0,0 +1,11 @@
+print("Tạo một cái pull request và merge request")
+print("Tạo một cái nội dung cho pull request")
+print("Tìm hiểu reset and revert")
+print("Nội dung cho revert")
+print("Cần thêm nội dung cho revert")
+print("Hom nay ngay 25/11/2024 va hom nay thu 2")
+print("Hom qua co mixi cup")
+print("All start voo dich")
+print("Teacher 3 ve 4")
+print("Doxi ve 2")
+
