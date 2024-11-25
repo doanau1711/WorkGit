db567f0198d2ead27b67cd4d3b66818ab139b6fe
DoanLongAu <audoan@gmail.com>
2024-11-25 10:26:40 +0700

    Modified file15_revert

--- file15_revert_nhanh_test.py
+++ file15_revert_nhanh_test.py
@@ -4,4 +4,4 @@ print("Tìm hiểu reset and revert")
 print("Nội dung cho revert")
 print("Cần thêm nội dung cho revert")
 print("Hom nay ngay 25/11/2024 va hom nay thu 2")
-
+print("Hom qua co mixi cup")
